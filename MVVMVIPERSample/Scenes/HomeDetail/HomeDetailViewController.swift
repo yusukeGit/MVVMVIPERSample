dd2da33d60db8b899d28f117b04e20302a1d29d1
y-watanabe <y-watanabe@cries.co.jp>
2021-05-10 21:08:47 +0900

    遷移先のHomeDetail Viewを追加

--- MVVMVIPERSample/Scenes/HomeDetail/HomeDetailViewController.swift
+++ MVVMVIPERSample/Scenes/HomeDetail/HomeDetailViewController.swift
@@ -5,4 +5,13 @@
 //  Created by yusuke watanabe on 2021/05/07.
 //
 
-import Foundation
+import UIKit
+
+final class HomeDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        label.text = "HomeDetailViewController"
+        view.addSubview(label)
+    }
+}
